2fcacf4f2fe3ece699101577c73ec5a065753ad3
nafis585 <princenafistoure@gmail.com>
2025-08-10 16:19:17 +0000

    fix: Load dotenv conditionally in development environment

--- util/db.ts
+++ util/db.ts
@@ -1,14 +1,16 @@
 import mysql from 'mysql2';
-import * as dotenv from 'dotenv';
 
-dotenv.config();
+// Charge dotenv seulement en DEV (local)
+if (process.env.NODE_ENV !== 'production') {
+  import('dotenv').then(dotenv => dotenv.config());
+}
 
 const db = mysql.createPool({
-  host: process.env.DB_HOST ,
+  host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT) || 3306,
-  user: process.env.DB_USER ,
-  password: process.env.DB_PASSWORD ,
-  database: process.env.DB_NAME ,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
